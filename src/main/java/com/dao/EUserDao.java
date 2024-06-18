fda2022b985ff389eeecb31765a89659a4745ca7
naitikjpatel <njpatel20031011@gmail.com>
2024-06-18 16:57:34 +0530

    add details

--- src/main/java/com/dao/EUserDao.java
+++ src/main/java/com/dao/EUserDao.java
@@ -6,6 +6,11 @@ import org.springframework.stereotype.Repository;
 
 import com.bean.EUserBean;
 
+//when we create a Dao class and we need that class object from the spring then we need to configure that using @Repository annotation.
+//For the In-built class we not need to write @Repository there is already mention. for an example like : JdbcTemplate : JdbcTemplate is a class of spring jdbc that is in-built class so we not need to write @Repository .just we need to use whenever we want to create the object of that then we need to use @Autowired:using @Autowired annotation we make the object using singleTon design pattern
+//in springBoot project we don't have the any xml file for the configuration
+//@Autowired is like : getBean() : to create an object
+
 @Repository
 public class EUserDao {
 
