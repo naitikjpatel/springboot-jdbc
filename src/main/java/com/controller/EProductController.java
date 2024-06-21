b1a04403d0b0104a56bbf4fb698a0209563f5568
naitikjpatel <njpatel20031011@gmail.com>
2024-06-21 16:05:19 +0530

    list products

--- src/main/java/com/controller/EProductController.java
+++ src/main/java/com/controller/EProductController.java
@@ -1,7 +1,10 @@
 package com.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -30,5 +33,14 @@ public class EProductController {
 		// dao insert
 		dao.addProduct(bean);
 		return "Welcome";
+	}
+	
+	@GetMapping("/products")
+	public String listProducts(Model model) {
+		
+		//call method : getAllProducts
+		List<EProductBean> products=dao.getAllProducts();
+		model.addAttribute("products",products);
+		return "EcomListProducts";
 	}
 }
--- src/main/java/com/dao/EProductDao.java
+++ src/main/java/com/dao/EProductDao.java
@@ -1,11 +1,14 @@
 package com.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.bean.EProductBean;
-import com.bean.EUserBean;
+
 
 @Repository
 public class EProductDao {
@@ -16,5 +19,11 @@ public class EProductDao {
 	
 	public void addProduct(EProductBean bean) {
 		stmt.update("insert into products (productName,category,qty,price) values (?,?,?,?)",bean.getProductName(),bean.getCategory(),bean.getQty(),bean.getPrice());
+	}
+	
+	public List<EProductBean> getAllProducts() {
+																//BeanPropertyRowMapper e nava bean banavi jashe for every row mate
+		List<EProductBean> list= stmt.query("select * from products", new BeanPropertyRowMapper<EProductBean>(EProductBean.class));
+		return list;
 	}
 }
